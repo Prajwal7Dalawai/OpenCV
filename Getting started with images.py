f6c0277f3b1185e32007b4e336d1cc6062240823
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-03 20:56:51 +0530

    Reading image and writing

--- Getting started with images.py
+++ Getting started with images.py
@@ -2,5 +2,10 @@ import cv2
 img= cv2.imread('data/lena.jpg',-1)
 print(img)
 cv2.imshow('image',img)  #Print the image pixel matrix
-cv2.waitKey(5000)    #Method to hold the opened window until certain time
-cv2.destroyAllWindows()   #Destroy all the windows created
+k = cv2.waitKey(0)    #Method to hold the opened window until certain time(in ms)
+
+if k==27 and 0xFF:
+   cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('image_lena.png',img)  #Writing the image, also this method is used to save the image in local machine
+    cv2.destroyAllWindows()   #Destroy all the windows created
