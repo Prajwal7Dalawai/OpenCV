b144f5ba6e19538fe3f7aecb9869b639fc9a980c
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-06 19:12:27 +0530

    Changes

--- Object detection and tracking.py
+++ Object detection and tracking.py
@@ -8,16 +8,16 @@ cv2.namedWindow("tracking")
 
 while True:
     frame = cv2.imread('data/smarties.png')
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Fix: Convert to HSV instead of GRAY
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  #Converting to HSV colour
 
-    l_b = np.array([110, 50, 50])
-    u_b = np.array([130, 255, 255])
+    l_b = np.array([110, 50, 50]) #lower base colour blue
+    u_b = np.array([130, 255, 255]) #higher base colour blue
     
-    mask = cv2.inRange(hsv, l_b, u_b)
+    mask = cv2.inRange(hsv, l_b, u_b)   #creating the mask of the image
     ress = cv2.bitwise_and(frame,frame,mask=mask)
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
-    cv2.imshow('res', ress)  # Fix: Correct the function name to cv2.imshow
+    cv2.imshow('res', ress) 
     
     key = cv2.waitKey(1)
     if key == ord('q'):
