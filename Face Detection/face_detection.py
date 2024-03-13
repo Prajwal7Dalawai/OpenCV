71481f128b7400d59673de775bea4a5113fe0488
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-13 19:49:29 +0530

    Face image detection

--- Face Detection/face_detection.py
+++ Face Detection/face_detection.py
@@ -4,8 +4,13 @@ of a particular object (i.e., a face or a car), called positive examples, that a
 negative examples - arbitrary images of the same size.
 #'''
 import cv2
-img = cv2.imread('data/test.png')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('test.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray,1.1,4)
 
-
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+cv2.imshow('face',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
