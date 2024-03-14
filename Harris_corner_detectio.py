b07616f54164f91ebc32a8e973a819bb0590baf3
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-14 19:33:50 +0530

    HArris Corner Detection

---
+++ Harris_corner_detectio.py
@@ -0,0 +1,5 @@
+'''#Haris Corner Detector
+1.Determine which windows produce very large variations in intensity when moved in both X and Y directions.
+2.With each such window found, a score R is computed.
+3.After applying a threshold to this score, important corners are selected and marked.
+#'''
