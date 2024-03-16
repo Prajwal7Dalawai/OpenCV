040239da62bdbb4f03bd0daacf98fea80144856f
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-16 23:40:56 +0530

    Meanshift in OpenCV

--- meanshift_in_opencv.py
+++ meanshift_in_opencv.py
@@ -1,6 +1,12 @@
 import numpy as np
 import cv2
 cap = cv2.VideoCapture('data/traffic.webm')
+'''#
+1.Take the first frame of the window.
+2.Set up the initial location of window.
+3.Set up the ROI of tracking
+4.Set up the termination criteria, either 10 iteration or move by one point.
+#'''
 while(1):
         ret, frame = cap.read()
         if ret == True:
