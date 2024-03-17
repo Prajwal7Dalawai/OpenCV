a999417057fc76b97d5a74be16eac3e709e8b5e1
Prajwal S Dalawai <prajwaldalawai123@gmail.com>
2024-03-17 18:22:24 +0530

    Reading text from images using Python

--- ocr.py
+++ ocr.py
@@ -1,5 +1,6 @@
 import cv2
-import pytesseract as py
+import tesseract 
+from pytesseract import pytesseract as py
 
 def ocr_core(img):
     text = py.image_to_string(img)
@@ -11,4 +12,4 @@ img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 img= cv2.medianBlur(img,5)
 img = cv2.threshold(img,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-print(ocr_core(img))
+print('yes',ocr_core(img))
